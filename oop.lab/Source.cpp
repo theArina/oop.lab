986653a0fd3b34a30d10c95cb912c002f95cf3e6
Tom <ghost.with.soul@gmail.com>
2017-11-16 13:26:30 +0700

    git testing

---
+++ oop.lab/Source.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <windowsx.h>
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	// получаем идентификатор окна
+	HWND hwnd = GetConsoleWindow();
+	// получаем контекст отображения
+	HDC hdc = GetDC(hwnd);
+	RECT rt;
+	char buf[100];
+	// устанавливаем цвет фона
+	SetBkColor(hdc, RGB(0, 0, 0));
+	// устанавливаем цвет текста
+	SetTextColor(hdc, RGB(255, 0, 0));
+	// создаем красное перо
+	HPEN hRedPen = CreatePen(PS_SOLID, 5, RGB(255, 0, 0));
+	// и выбираем его в контекст отображения,
+	// сохраняя предыдущее перо
+	HPEN hOldPen = SelectPen(hdc, hRedPen);
+	// создаем зеленую кисть
+	HBRUSH hGreenBrush = CreateSolidBrush(RGB(0, 255, 0));
+	20;
+	// и выбираем ее в контекст отображения,
+	// сохраняя предыдущую кисть
+	HBRUSH hOldBrush = SelectBrush(hdc, hGreenBrush);
+	// выводим строку стандартными средствами
+	printf("Graphics in Console Window.");
+	do {
+		// получаем размер окна
+		GetClientRect(hwnd, &rt);
+		// формируем выводимую строку
+		sprintf(buf, "Размер окна %d на %d пикселей",
+			rt.right, rt.bottom);
+		// выводим строку графическими средствами
+		TextOutA(hdc, 10, 10, buf, strlen(buf));
+		// рисуем закрашенный эллипс
+		Ellipse(hdc, 10, 30, rt.right - 10, rt.bottom - 10);
+	} while (_getch() != 27); // при нажатии любой клавиши
+							  // (кроме Esc) перерисовываем изображение,
+							  // изображение изменится, если изменились размеры окна,
+							  // нажатие Esc – выход
+							  // выбираем в контекст отображения предыдущее перо
+	SelectPen(hdc, hOldPen);
+	// выбираем в контекст отображения предыдущую кисть
+	SelectBrush(hdc, hOldBrush);
+	// удаляем красное перо
+	DeletePen(hRedPen);
+	// удаляем зеленую кисть
+	DeleteBrush(hGreenBrush);
+	// освобождаем контекст отображения
+	ReleaseDC(hwnd, hdc);
+}
